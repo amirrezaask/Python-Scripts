cd5bd123cfcb6105ff251c8c7a995b2710b0690b
amirreza_ask77 <raskarpour@gmail.com>
2017-03-08 20:44:49 +0330

    Two Simple Scripts Very Good For Learning To Use Mechanize

---
+++ CheckHW.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import requests
+import sys
+import re
+from mechanize import Browser
+import Cookie
+import cookielib
+from bs4 import BeautifulSoup
+student_page = requests.get("http://kish.sharif.edu/bs_email_tic.php")
+#student_id = re.findall("([95])\d+",student_page.text)
+#student_id = ''.join(chr(i) for i in student_id)
+
+
+#print student_page.text
+print "Once Again Haj_amir In the Seen"
+cookiejar =cookielib.LWPCookieJar()
+br = Browser()
+br.set_handle_robots(False)
+br.set_cookiejar(cookiejar)
+br.addheaders = [("User-agent","Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13")]
+sign_in = br.open("http://cw.sharif.ir/login/index.php")
+br.select_form(nr=0)
+br["username"]= sys.argv[1]
+br["password"]= sys.argv[1]
+logged_in = br.submit()
+br2 = Browser()
+br2.set_handle_robots(False)
+br2.set_cookiejar(cookiejar)
+br2.addheaders = [("User-agent","Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13")]
+sign_in2 = br2.open("http://cw.sharif.ir/mod/assign/view.php?id=68716")
+page = sign_in2.read()
+soup = BeautifulSoup(page,"html.parser")
+if ".pdf" in page:
+    print "Uploaded !"
+    for link in br2.links(url_regex=".pdf"):
+        br2.retrieve(link.url,"hw_{}.pdf".format(sys.argv[1]))
+
+else:
+    print "We Are Anonymous, We Are Legion, We Do Not Forgive, We Do Not Forget"
+    print "Dosstemoon hanooz upload nakarde !!"
